77e69fa408d532924d4c42900a07cd7517e6a82c
camberk <cameronberkowitz@icloud.com>
2021-11-17 12:26:05 -0500

    firebase not working

--- js/enterWorkouts.js
+++ js/enterWorkouts.js
@@ -1,13 +1,36 @@
 var enterView = new Vue({
     el: '#app',
     data: {
-        enteredText: '',
+        workoutType: '',
+        weight: '',
+        reps: '',
+        sets: '',
+        date: '',
+        wt: false,
+        w: false,
+        r: false,
+        s: false,
+        d: false,
+        testdb: '',
     },
     methods: {
-        keyPressed: function(event) {
-            if (event.key == "Enter") {
-                console.log("enter pressed");
+        enterData: function(event) {
+            if (event.key == "Enter" && event.currentTarget.id == "workouttype") {
+                this.wt = true;
             }
+            if (event.key == "Enter" && event.currentTarget.id == "weight") {
+                this.w = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "numberreps") {
+                this.r = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "numbersets") {
+                this.s = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "date") {
+                this.d = true;
+            }
+            console.log(x);
         }
     }
 })
