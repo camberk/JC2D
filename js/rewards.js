49feb7198973fe9017eed9b1ce05c522985140c4
oneilljp <oneilljp@protonmail.com>
2021-11-17 13:06:15 -0500

    Initial rewards commit

---
+++ js/rewards.js
@@ -0,0 +1,14 @@
+let rewardView = new Vue({
+  el: '#reward-app',
+  data: {
+    search: '',
+    rewards: [],
+  },
+  methods: {
+    onSubmit: function(e) {
+      if (e.keycode == "Enter") {
+
+      }
+    }
+  }
+})
