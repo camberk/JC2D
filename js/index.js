4968514db3012c307f7a1badb7ab5ad1262b9063
Julia <jwoelmer@umich.edu>
2021-11-20 15:45:09 -0500

    Got chart to show up with hard-coded values

--- js/index.js
+++ js/index.js
@@ -1,16 +1,60 @@
-var mainView = new Vue({
-    el: '#app',
-    data: {
-        test: "Hello World",
-    },
-    methods: {
-        onPressed: function() {
-            if (this.test == "Hello World") {
-                this.test = "Pressed";
+Chart.defaults.global.defaultFontSize = 30;
+Chart.defaults.global.defaultFontFamily = 'Tienne';
+
+$(document).ready(function () {
+    var xValues =  ["You", "Frank", "Sam", "Ava", "Annie"];
+    var yValues = [55, 49, 44, 24, 15];
+    var barColors = ["red", "green","blue","orange","brown"];
+    var ctx = document.getElementById('leaderboardChart').getContext('2d');
+    var leaderboardChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+            labels: xValues,
+            datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+        },
+        options: {
+            legend: {display: false},
+            title: {
+                display: true,
+                text: "Today's Leaderboard",
+                fontSize: 50,
             }
-            else {
-                this.test = "Hello World";
-            }
+            
         }
-    }
-})
+  });
+    
+});
+
+// var mainView = new Vue({
+//     el: '#app',
+//     data: {
+//         leaderboardChart: new Chart(this.ctx, {
+//             type: "bar",
+//             data: {
+//                 labels: this.xValues,
+//                 datasets: [{
+//                 backgroundColor: this.barColors,
+//                 data: this.yValues
+//             }]
+//             },
+//             options: {
+//                 legend: {display: false},
+//                 title: {
+//                     display: true,
+//                     text: "Today's Leaderboard"
+//                 }
+//             }
+//         }),
+//         xValues: ["You", "Frank", "Sam", "Ava", "Annie"],
+//         yValues: [55, 49, 44, 24, 15],
+//         barColors: ["red", "green","blue","orange","brown"],
+//         ctx:  this.$refs.leaderboardChart.getContext('2d'),
+//     },
+//     methods: {
+
+        
+//     }
+// })
