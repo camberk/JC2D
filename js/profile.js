72ac97e32600c09f9b27e1e9a030fecf19b440a5
DanJH <daniel.j.heidorn@gmail.com>
2021-11-23 14:54:34 -0500

    Finish achievement section Add workout history Fix flex scroll

--- js/profile.js
+++ js/profile.js
@@ -5,33 +5,48 @@ var profileView = new Vue({
     el: "#profile",
     data: {
         //store data for current profile
-        profile: {
-            profileName: "John Smith",
-            profileNumber: 123,
-            profileAge: 32,
-            profileAchievements: [
-                {
-                    achievementName: "Test Achievement 1",
-                    achievementPictureUrl: "https://i.imgur.com/wDC5ejT.jpg"
-                },
-                {
-                    achievementName: "Test Achievement 2",
-                    achievementPictureUrl: "https://i.imgur.com/EU5bT1k.jpg"
-                }],
-            profilePhotoUrl: "https://i.stack.imgur.com/l60Hf.png"
-        }
-
+        profileName: "John Smith",
+        profileNumber: 23414,
+        profileAge: 32,
+        profileAchievements: [
+            {
+                achievementName: "From Athens to Marathon",
+                achievementPictureUrl: "https://www.pinclipart.com/picdir/big/2-24050_11-marathon-clipart-png-transparent-png.png",
+                achievementDescription: "Run a total 26.2 miles in a week"
+            },
+            {
+                achievementName: "Varisty  Athlete",
+                achievementPictureUrl: "https://pngimage.net/wp-content/uploads/2018/05/champ-png-6.png",
+                achievementDescription: "Complete 30 days of workouts"
+            },
+            {
+                achievementName: "Tear It Up!",
+                achievementPictureUrl: "https://www.clipartmax.com/png/full/80-802036_skateboard-clipart-transparent-skateboarder-pdf.png",
+                achievementDescription: "Skate, ski, or ride for the first time"
+            },
+            {
+                achievementName: "Junior Varisty",
+                achievementPictureUrl: "https://www.pinclipart.com/picdir/big/459-4590529_muscle-flex-outline-comments-flexing-arm-no-background.png",
+                achievementDescription: "Complete a week of workouts"
+            },
+            {
+                achievementName: "Blast Off",
+                achievementPictureUrl: "https://i.imgur.com/XNmBnWU.png",
+                achievementDescription: "Complete your first workout"
+            }],
+        profilePhotoUrl: "https://i.stack.imgur.com/l60Hf.png",
+        profileHeaderPhoto: "https://i.imgur.com/OOSF8qV.png"
     }
     //created() {
-    //on page loading, fetch profile name, profile photo
-    /*axious.get('/user?ID=123').then(
-        function(response){
-            this.profileName = response.name;
-            this.profilePhotoUrl = response.profilePhotoUrl;
-        }
-    ).catch(function(error){
-        console.log(error);
-    });*/
+        //on page loading, fetch profile name, profile photo
+        /*axious.get('/user?ID=123').then(
+            function(response){
+                this.profileName = response.name;
+                this.profilePhotoUrl = response.profilePhotoUrl;
+            }
+        ).catch(function(error){
+            console.log(error);
+        });*/
     //}, 
-
+    
 });
