1e1a23c59fbaefa250a60f3b93d0c66cd33f75cf
camberk <cameronberkowitz@icloud.com>
2021-11-24 00:43:06 -0500

    fake db working

--- js/enterWorkouts.js
+++ js/enterWorkouts.js
@@ -1,24 +1,7 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-import { getDatabase, ref, set, push} from "https://www.gstatic.com/firebasejs/9.4.0/firebase-database.js";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC_yI7DO1tu4-HAsNfPC-NC5yrv5bU5DcE",
-  authDomain: "jc2d-c4ce4.firebaseapp.com",
-  databaseURL: "https://jc2d-c4ce4-default-rtdb.firebaseio.com",
-  projectId: "jc2d-c4ce4",
-  storageBucket: "jc2d-c4ce4.appspot.com",
-  messagingSenderId: "969572763824",
-  appId: "1:969572763824:web:d67c3541186d49e263d375"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 const uniqName = 'camberk'
 const d = new Date();
+
+let dataTbl = {};
 
 var enterView = new Vue({
     el: '#app',
@@ -50,16 +33,27 @@ var enterView = new Vue({
             }
         },
         writeUserData: function() {
-            const db = getDatabase();
             var data = {
                 'weight': this.weight,
                 'reps': this.reps,
                 'sets': this.sets,
                 'date': Date(),
             }
-            let userRef = ref(db, [uniqName] + "_" + this.workoutType);
-            push(userRef, [data]);              
-
+            if (!(dataTbl[uniqName])) {
+                let temp = {};
+                temp[this.workoutType] = [data]
+                dataTbl[uniqName]= temp;
+                
+            }
+            else {
+                if (!(dataTbl[uniqName][this.workoutType])) {
+                    dataTbl[uniqName][this.workoutType] = [data];
+                }
+                else {
+                    dataTbl[uniqName][this.workoutType].push(data);
+                }
+            }
+            console.log(dataTbl);
         },
         
     }
