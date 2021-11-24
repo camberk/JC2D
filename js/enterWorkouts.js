5109ebded9501ac31cb776de7e508c792a72e290
camberk <cameronberkowitz@icloud.com>
2021-11-24 13:03:19 -0500

    workout details

--- js/enterWorkouts.js
+++ js/enterWorkouts.js
@@ -1,16 +1,60 @@
+const uniqName = 'camberk'
+const d = new Date();
+
+let dataTbl = {};
+
 var enterView = new Vue({
     el: '#app',
     data: {
-        test: "Hello World",
+        workoutType: '',
+        weight: '',
+        reps: '',
+        sets: '',
+        date: d.toDateString(),
+        wt: false,
+        w: false,
+        r: false,
+        s: false,
+        testdb: '',
     },
     methods: {
-        onPressed: function() {
-            if (this.test == "Hello World") {
-                this.test = "Pressed";
+        enterData: function(event) {
+            if (event.key == "Enter" && event.currentTarget.id == "workouttype") {
+                this.wt = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "weight") {
+                this.w = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "numberreps") {
+                this.r = true;
+            }
+            if (event.key == "Enter" && event.currentTarget.id == "numbersets") {
+                this.s = true;
+            }
+        },
+        writeUserData: function() {
+            var data = {
+                'weight': this.weight,
+                'reps': this.reps,
+                'sets': this.sets,
+                'date': Date(),
+            }
+            if (!(dataTbl[uniqName])) {
+                let temp = {};
+                temp[this.workoutType] = [data]
+                dataTbl[uniqName]= temp;
+                
             }
             else {
-                this.test = "Hello World";
+                if (!(dataTbl[uniqName][this.workoutType])) {
+                    dataTbl[uniqName][this.workoutType] = [data];
+                }
+                else {
+                    dataTbl[uniqName][this.workoutType].push(data);
+                }
             }
-        }
+            console.log(dataTbl);
+        },
+        
     }
 })
